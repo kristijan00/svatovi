fdb22a0f38170364a97fdb12d31e9e9dbaec29cd
Kristijan Đaković <kristijandjakovic10@gmail.com>
2025-04-12 20:37:58 +0200

    Updated the look of the page, added env variables and a workflow to deploy

--- main.js
+++ main.js
@@ -1,6 +1,31 @@
-var typeNumber = 30;
-var errorCorrectionLevel = 'Q';
-var qr = qrcode(typeNumber, errorCorrectionLevel);
-qr.addData('https://www.dropbox.com/request/oIawNDy9GLkXIrU4dJtP');
-qr.make();
-document.getElementById('placeHolder').innerHTML = qr.createImgTag();
+const fileInput = document.getElementById('fileInput');
+let uploadedFiles = [];
+import CONFIG from './config.js';
+
+const dbx = new Dropbox.Dropbox({
+  clientId: CONFIG.DROPBOX_CLIENT_ID,
+  clientSecret: CONFIG.DROPBOX_CLIENT_SECRET,
+  refreshToken: CONFIG.DROPBOX_REFRESH_TOKEN,
+  accessToken: CONFIG.DROPBOX_ACCESS_TOKEN,
+});
+
+fileInput.addEventListener('change', () => {
+  uploadedFiles.push(...fileInput.files);
+});
+
+const uploadFiles = () => {
+  uploadedFiles.forEach(file => {
+    dbx.filesUpload({
+      path: `/test/${file.name}`,
+      contents: file,
+    })
+      .then(response => {
+        console.log('Success:', response);
+        alert('Files uploaded successfully!');
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        alert('Error uploading files!');
+      });
+  });
+}
